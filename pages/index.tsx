a29c5aff20d03dfd90b6a5c41880021a97371ca0
Zhaomian Zhao <loisisar@outlook.com>
2023-01-12 16:00:57 +0000

    need more touch up on cards?

--- pages/index.tsx
+++ pages/index.tsx
@@ -22,7 +22,7 @@ export default function Home() {
         <div className={styles.description}>
           <Link
             href="https://medium.com/@zhaomianzhao/the-business-of-experience-c1af1c20bc69"
-            className={`underline font-extralight tracking-wide flex justify-center items-center gap-2 underline-offset-4 decoration-0 italic`}
+            className={`md:text-md text-sm underline font-extralight tracking-wide flex justify-center items-center gap-2 underline-offset-4 decoration-0 italic`}
           >
             &#8220; We all think time is all we have, whilst time is not
             represented in our memory.&#8221;
@@ -47,33 +47,62 @@ export default function Home() {
             >
               hotel management in Macau
             </Link>
-            , the Asia Las Vegas at the beginning. I hated casinmo mathematic at
-            that time yet slowly realising the treasure they have gave me.
+            , the Asia Las Vegas at the beginning. I hated casino mathematic at
+            that time yet slowly realising the treasure they had given me.
             <br />
             <br />
             First worked in Las Vegas Sands, Macau&lsquo;s first smart casino
-            project, I worked with data scientists from Stanford, Monash, LSE to{" "}
+            project, I assisted data scientists from Stanford, Monash, LSE to{" "}
             <Link
               href="https://www.thinkwithgoogle.com/intl/en-apac/future-of-marketing/creativity/sands-china-embraces-machine-learning-focus-high-value-consumers/"
               className="underline underline-offset-4 decoration-1"
             >
-              convert high value customers using Tensorflow.
+              convert high-value customers using Tensorflow.
             </Link>
             <br />
             <br />
+            Prior to my work in{" "}
+            <Link
+              href="https://www.hvs.com/"
+              className="underline underline-offset-4 decoration-1"
+            >
+              HVS China
+            </Link>{" "}
+            to write hospitality industry publications covering China and US
+            market for Chinese government and state-own companies, I worked in{" "}
             <Link
               href="https://www.page.com/home-page"
               className="underline underline-offset-4 decoration-1"
             >
               Page group
             </Link>{" "}
-            was where I hunted CMOs for Nike, Swaroski and alike, learnt sales
-            and some fundamentals of recruiting.
+            , learnt sales and fundamentals of recruiting by headhunting CMOs
+            for Nike, Swaroski and many other companies' operation in China.
+            Later on I joined{" "}
+            <Link
+              href="https://www.knightfrank.com.cn/en/about-us"
+              className="underline underline-offset-4 decoration-1"
+            >
+              Knight Frank South China Office
+            </Link>{" "}
+            as the 6th employee, given the responsibility to handle office
+            transactions and commercial real estate. Because I hadn't graduated
+            at that time, they called me "Head of Intern".
+            <br />
+            <br />I was fortunate to study Python and a light chunk of ML for
+            building financial portfolio in{" "}
+            <Link
+              href="https://www.henley.ac.uk/"
+              className="underline underline-offset-4 decoration-1"
+            >
+              Henley Business School
+            </Link>
+            . Other than those, I learnt everything else on my own.
           </p>
         </div>
 
         <div className={styles.grid}>
-          <div className="relative lg:static xl:pl-10">
+          <div className="relative ">
             <div className="absolute inset-x-[-50vw] -top-32 -bottom-48 [mask-image:linear-gradient(transparent,white,white)] dark:[mask-image:linear-gradient(transparent,white,transparent)] lg:left-[calc(50%+14rem)] lg:right-0 lg:-top-32 lg:-bottom-32 lg:[mask-image:none] lg:dark:[mask-image:linear-gradient(white,white,transparent)]"></div>
             <div className="relative">
               <Image
@@ -94,31 +123,32 @@ export default function Home() {
                 unoptimized
                 priority
               />
-              <div className="absolute inset-0 hover:inset-1 rounded-2xl bg-gradient-to-tr rotate-180 from-sky-300 via-sky-300/70 to-blue-300 opacity-10 blur-lg" />
-              <div className="absolute inset-0 hue-rotate-180 rotate-180 rounded-2xl bg-gradient-to-tr from-sky-300 via-sky-300/70 to-blue-300 opacity-10" />
-              <div className="relative  rounded-2xl bg-[#0A101F]/80 ring-1 ring-white/10 backdrop-blur">
-                <div className="absolute -top-px left-20 right-11 h-px bg-gradient-to-r from-sky-300/0 via-sky-300/70 to-sky-300/0" />
-                <div className="absolute -bottom-px rotate-180 left-11 right-20 h-px bg-gradient-to-r from-blue-400/0 via-blue-400 to-blue-400/0 " />
-                <div className="pl-4 pt-4 hover:from-blue-400/30 hover:via-blue-600 hover:to-blue-400/60">
-                  {" "}
-                  <Link
-                    href="https://medium.com/@zhaomianzhao"
-                    className=""
-                    target="_blank"
-                    rel="Zhaomian Zhao Public Blog"
-                  >
-                    <h2 className={`${inter.className} font-bold text-lg mb-1`}>
-                      Medium <span></span>
-                    </h2>
-                    <p
-                      className={`${inter.className} font-sm  font-extralight`}
-                    >
-                      Blog about a bunch of random things related to my work and
-                      sports. Marketing-ish.
-                    </p>
-                  </Link>
-                </div>
-              </div>
+            </div>
+          </div>
+          <div className="md:flex flex-auto">
+            <div className="m-5">
+              <Card
+                href="https://www.linkedin.com/in/loiszhao/"
+                title="LinkedIn"
+                description=""
+                rel=""
+              />
+            </div>
+            <div className="m-5">
+              <Card
+                href="https://medium.com/@zhaomianzhao"
+                title="Medium"
+                description=""
+                rel="Zhaomian Zhao Personal Blog"
+              />
+            </div>
+            <div className="m-5">
+              <Card
+                href="https://twitter.com/LoisZhao01"
+                title="Twitter"
+                description=""
+                rel="Zhaomian Zhao's Twitter"
+              />
             </div>
           </div>
         </div>
@@ -126,3 +156,35 @@ export default function Home() {
     </>
   );
 }
+
+const Card = ({
+  title,
+  description,
+  href,
+  rel,
+}: {
+  title: string;
+  description: string;
+  href: string;
+  rel: string;
+}) => {
+  return (
+    <>
+      <div className="w-full border-2 border-cyan-600/30 rounded-lg hover:shadow-md hover:shadow-sky-400/40">
+        <div className="p-2 md:p-4 hover:from-blue-400/30 hover:via-blue-600 hover:to-blue-400/60">
+          {" "}
+          <Link href={href} className="" target="_blank" rel={rel}>
+            <h2
+              className={`self-center justify-self-center font-bold text-lg mb-1`}
+            >
+              {title} <span></span>
+            </h2>
+            <p className={`justify-self-center font-sm  font-extralight`}>
+              {description}
+            </p>
+          </Link>
+        </div>
+      </div>
+    </>
+  );
+};
